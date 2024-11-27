716cccd28a52b3160365f633efe65b91477c79d7
Sumon <sksumon1410wwwe@gmail.com>
2024-11-27 09:17:12 +0600

    version 2

--- Simple.java
+++ Simple.java
@@ -2,6 +2,7 @@ package simple;
 
 class one{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello Java");
+     System.out.println("Hello Java565645678678768");  
     }  
 }  
